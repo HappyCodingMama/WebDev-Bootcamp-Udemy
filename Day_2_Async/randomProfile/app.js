bc4bd8889610777a59ba6ac7f352e987a433778d
HappyCodingMama <happycodingmama@gmail.com>
2020-09-15 00:25:19 +0900

    revising codes

--- Day_2_Async/randomProfile/app.js
+++ Day_2_Async/randomProfile/app.js
@@ -1,4 +1,4 @@
-const url = 'https://randomuser.me/api';
+/* const url = 'https://randomuser.me/api';
 
 const btn = document.querySelector('.btn');
 
@@ -25,4 +25,49 @@ btn.addEventListener('click', () => {
             .catch((error) => {
                   console.log('There has been a problem.', error);
             })
-})
+}) */
+
+
+
+const url = 'https://randomuser.me/api';
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+      fetch(url)
+            .then(handleErrors)
+            .then(parseJSON)
+            .then(updateProfile)
+            .catch(displayErrors)
+});
+
+
+function handleErrors(res) {
+      if (!res.ok) {
+            throw Error(res.status);
+      }
+      return res;
+}
+
+function parseJSON(res) {
+      return res.json().then(function (parsedData) {
+            return parsedData.results[0]
+      })
+}
+
+function updateProfile(data) {
+
+      const profileImg = document.querySelector('#profile-img');
+      profileImg.src = `${data.picture.large}`
+
+      document.querySelector('#first-name').innerHTML = `${data.name.first}`
+      document.querySelector('#last-name').innerHTML = `${data.name.last}`
+      document.querySelector('#username').innerHTML = `${data.login.username}`
+      document.querySelector('#email').innerHTML = `Email: ${data.email}`
+      document.querySelector('#city').innerHTML = `City: ${data.location.city}`
+
+}
+
+function displayErrors(error) {
+      console.log('There has been a problem.', error);
+}
