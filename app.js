1473dbf7e51e7929a57e68d676b06b2c91474c2b
HappyCodingMama <happycodingmama@gmail.com>
2020-09-18 22:07:45 +0900

    summarize what I learned

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
-var url = 'https://opentdb.com/api.php?amount=1';
-axios.get(url)
-      .then(function (res) {
-            console.log(res.data.results[0].question);
-      })
-      .catch(function () {
-            console.log('ERR');
-      })
+// https://ron-swanson-quotes.herokuapp.com/v2/quotes
+
+
+const XHR = document.querySelector('#xhr');
+const Fetch = document.querySelector('#fetch');
+const jQuery = document.querySelector('#jquery');
+const Axios = document.querySelector('#axios');
+
+const quote = document.querySelector('#quote');
+
+const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+// xhr
+XHR.addEventListener('click', function () {
+      let XHR = new XMLHttpRequest();
+
+      XHR.onreadystatechange = function () {
+            if (XHR.readyState === 4 && XHR.status === 200) {
+                  let data = JSON.parse(XHR.responseText)[0];
+                  quote.innerText = `"${data}"`;
+            }
+      }
+
+      XHR.open('GET', url);
+      XHR.send();
+})
+
+//fetch
+Fetch.addEventListener('click', function () {
+      fetch(url)
+            .then((req) => {
+                  return req.json()
+            })
+            .then((data) => {
+                  quote.innerText = `"${data[0]}"`
+            })
+            .catch((error) => {
+                  console.log(error);
+            })
+})
+
+//jQuery
+$(jquery).click(function () {
+      $.getJSON(url)
+            .done(function (data) {
+                  $(quote).text(`"${data[0]}"`);
+            })
+            .fail(function () {
+                  console.log('There was a problem');
+            })
+})
+
+//axios
+Axios.addEventListener('click', function () {
+      axios.get(url)
+            .then((res) => {
+                  quote.innerText = `"${res.data[0]}"`
+            })
+            .catch((err) => {
+                  console.log(err);
+            })
+})
+
+
